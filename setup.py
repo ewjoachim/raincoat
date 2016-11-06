4b79c7b2712dcc37a4062a480cc0b289c2080f86
Joachim Jablon <ewjoachim@gmail.com>
2016-11-06 16:29:57 +0100

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.4.0'
+version = '0.4.1'
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
