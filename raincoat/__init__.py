3f1b6d8495ca7a308ea2f5100dff2b99f3995881
Joachim Jablon <ewjoachim@gmail.com>
2017-02-22 21:41:09 +0100

    Add help text

--- raincoat/__init__.py
+++ raincoat/__init__.py
@@ -13,6 +13,12 @@ CONTEXT_SETTINGS = {'help_option_names': ['-h', '--help']}
 @click.option('--path', default=".", help='Path to analyze (default is "." )')
 def main(path):
 @click.command(context_settings=CONTEXT_SETTINGS)
+    """
+    Analyze your code to find outdated copy-pasted snippets.
+    "Raincoat has you covered when your code is not DRY."
+
+    Full documentation at http://raincoat.readthedocs.io/en/latest/
+    """
     raincoat = Raincoat()
 
     errors = list(raincoat.raincoat(path=path))
