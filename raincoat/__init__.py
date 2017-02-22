1e723b08ca6eb46ec6f27c503bb55113c4ac5964
Joachim Jablon <ewjoachim@gmail.com>
2017-02-22 21:41:38 +0100

    Support multiple pathes

--- raincoat/__init__.py
+++ raincoat/__init__.py
@@ -10,9 +10,9 @@ __version__ = "0.6.0"
 
 CONTEXT_SETTINGS = {'help_option_names': ['-h', '--help']}
 
-@click.option('--path', default=".", help='Path to analyze (default is "." )')
 def main(path):
 @click.command(context_settings=CONTEXT_SETTINGS)
+@click.argument('path', nargs=-1, type=click.Path(exists=True))
     """
     Analyze your code to find outdated copy-pasted snippets.
     "Raincoat has you covered when your code is not DRY."
@@ -20,8 +20,10 @@ def main(path):
     Full documentation at http://raincoat.readthedocs.io/en/latest/
     """
     raincoat = Raincoat()
+    if not path:
+        path = ["."]
 
-    errors = list(raincoat.raincoat(path=path))
+    errors = list(raincoat.raincoat(path=element) for element in path)
     for error, match in errors:
         click.echo(match)
         click.echo(error)
